b8913d4e68d22a3eb93d19bfa432e6ae3fd23e3a
enesaliovur <enesaliovur@gmail.com>
2023-03-21 23:21:57 +0300

    creating the edit view

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -35,7 +35,20 @@ extension DailyScrum {
         }
         
     }
+    
+    struct Data {
+        var title: String = ""
+        var attendees: [Attendee] = []
+        var lengthInMinutes: Double = 5
+        var theme: Theme = .seafoam
+    }
+    
+    var data: Data {
+        Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
+    }
 }
+
+
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
--- Scrumdinger/View/DetailView.swift
+++ Scrumdinger/View/DetailView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct DetailView: View {
     let scrum: DailyScrum
     
+    @State private var isPresentingEditView = false;
+    
     var body: some View {
         let theme = scrum.theme
         List {
@@ -45,6 +47,25 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar(content: {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        })
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement:.cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+
+            }
+        }
 
     }
 }
